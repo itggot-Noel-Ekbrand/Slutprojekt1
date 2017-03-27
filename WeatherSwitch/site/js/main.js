c1e4b91c7a323b53d858a379809ad0288da4fa6a
itggot-Noel-Ekbrand <noel.ekbrand@itggot.se>
2017-03-28 00:24:20 +0200

    hello very good work yes

---
+++ WeatherSwitch/site/js/main.js
@@ -0,0 +1,10 @@
+function menuAnimation() {
+	document.querySelector(".menu").classList.add("menuAnimation");
+	document.querySelector(".shade").classList.add("shadeAnimation");
+
+}
+
+function menuRemove() {
+	document.querySelector(".menu").classList.remove("menuAnimation");
+	document.querySelector(".shade").classList.remove("shadeAnimation");
+}
