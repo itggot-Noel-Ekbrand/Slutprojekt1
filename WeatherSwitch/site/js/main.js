45dbe1f7bb87f5de8210c75611b2a05f18051e63
itggot-Noel-Ekbrand <noel.ekbrand@itggot.se>
2017-04-05 14:38:00 +0200

    wednsday

--- WeatherSwitch/site/js/main.js
+++ WeatherSwitch/site/js/main.js
@@ -1,10 +1,25 @@
 function menuAnimation() {
 	document.querySelector(".menu").classList.add("menuAnimation");
 	document.querySelector(".shade").classList.add("shadeAnimation");
-
 }
 
 function menuRemove() {
 	document.querySelector(".menu").classList.remove("menuAnimation");
 	document.querySelector(".shade").classList.remove("shadeAnimation");
+}
+
+function confirmWeather() {
+  document.querySelector(".confirm").classList.toggle("displaynone");
+}
+
+function removeConfirm() {
+  document.querySelector(".confirm").classList.remove("displaynone");
+}
+
+function removeareaConfirm() {
+  document.querySelector(".confirm-area").classList.remove("displaynone");
+}
+
+function confirmareaWeather() {
+  document.querySelector(".confirm-area").classList.toggle("displaynone");
 }
