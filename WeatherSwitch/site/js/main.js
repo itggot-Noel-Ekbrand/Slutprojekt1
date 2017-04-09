5e5fe794b1daae3944415a7493c48afb21d74436
itggot-Noel-Ekbrand <noel.ekbrand@itggot.se>
2017-04-09 16:58:28 +0200

    hello

--- WeatherSwitch/site/js/main.js
+++ WeatherSwitch/site/js/main.js
@@ -33,6 +33,21 @@ function searchRemove() {
 	document.querySelector(".square").classList.remove("displaynone");
 }
 
+function toggleFullScreen() {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);
+  }
+}
+
 function initAutocomplete() {
         var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -33.8688, lng: 151.2195},
